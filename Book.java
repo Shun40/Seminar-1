ea7dd1edc19834adff419fd4a5be594f6a72c8c6
Shun40 <shun.no40@gmail.com>
2012-10-25 23:57:44 +0900

    Book class source.

---
+++ Book.java
@@ -0,0 +1,15 @@
+
+public class Book {
+	private String name;
+	public Book(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+}
+
+/* メモ
+ * Book型の中身を定義
+ * ここでは本の名前情報のみを含む
+ */
